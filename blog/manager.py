3ac129688fae523b567a582bb7f6365f307910b4
Pengfei Xue <pengphy@gmail.com>
2012-11-25 22:30:02 +0800

    add manage script

---
+++ blog/manager.py
@@ -0,0 +1,14 @@
+from flask.ext.script import Manager, Server
+from main import app
+
+manager = Manager(app)
+
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0')
+)
+
+if __name__ == "__main__":
+    manager.run()
