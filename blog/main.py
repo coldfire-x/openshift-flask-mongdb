5ef6fe1580a92c18d46baddf3f8041c7bc8df3f4
Pengfei Xue <pengphy@gmail.com>
2012-11-25 22:29:49 +0800

    add mongodb configuration

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,22 @@
+# -*- coding: utf8 -*-
+import os
+
 from flask import Flask, render_template
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.debug = True
+
+# mongodb configuration
+app.config['MONGODB_DB'] = os.environ['OPENSHIFT_APP_NAME']
+app.config['MONGODB_USERNAME'] = os.environ['OPENSHIFT_MONGODB_DB_USERNAME']
+app.config['MONGODB_PASSWORD'] = os.environ['OPENSHIFT_MONGODB_DB_PASSWORD']
+app.config['MONGODB_HOST'] = os.environ['OPENSHIFT_MONGODB_DB_HOST']
+app.config['MONGODB_PORT'] = os.environ['OPENSHIFT_MONGODB_DB_PORT']
+
+# get db connection
+db = MongoEngine(app)
+
 
 @app.route("/")
 def hello():
