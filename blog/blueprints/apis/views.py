50fdbffed1e0d242df4f4e169ab6fc6aaed7cfb1
Pengfei Xue <pengphy@gmail.com>
2013-05-07 19:03:04 +0800

    add autocomplete function for slug when modify or create a new post

--- blog/blueprints/apis/views.py
+++ blog/blueprints/apis/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf8 -*-
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask.views import MethodView
 from werkzeug.contrib.cache import SimpleCache
 
+from blueprints.posts.models import Post
 from utilities import get_cnbeta_feed
 
 apis = Blueprint('apis', __name__)
@@ -20,4 +21,16 @@ class CnbetaFeedView(MethodView):
 
         return jsonify(tips=tips)
 
-apis.add_url_rule('/cnbeta.json', view_func=CnbetaFeedView.as_view('callpoint'))
+
+class TagAutocomplete(MethodView):
+    def get(self):
+        term = request.args.get('term', None)
+        items = Post.objects(slug__icontains=term).only('slug')
+        return jsonify(items=[item.slug for item in items])
+
+
+apis.add_url_rule('/cnbeta.json',
+    view_func=CnbetaFeedView.as_view('cnbetacallpoint'))
+
+apis.add_url_rule('/tagautocomplete.json',
+    view_func=TagAutocomplete.as_view('tagautocomplete'))
